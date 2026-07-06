2e79e402baaa3510a52b1511b0acb5840e452021
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-07-06 16:19:30 +0000

    TCP socket created successfully

---
+++ build/CMakeFiles/proxy.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/akshat_chitransh/projects/High-Performance-Multithreaded-HTTP-Proxy-Server-C-/src/main.cpp" "CMakeFiles/proxy.dir/src/main.cpp.o" "gcc" "CMakeFiles/proxy.dir/src/main.cpp.o.d"
+  "/home/akshat_chitransh/projects/High-Performance-Multithreaded-HTTP-Proxy-Server-C-/src/server.cpp" "CMakeFiles/proxy.dir/src/server.cpp.o" "gcc" "CMakeFiles/proxy.dir/src/server.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/proxy.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/proxy.dir/src/main.cpp.o: \
+ /home/akshat_chitransh/projects/High-Performance-Multithreaded-HTTP-Proxy-Server-C-/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/akshat_chitransh/projects/High-Performance-Multithreaded-HTTP-Proxy-Server-C-/include/server.hpp
